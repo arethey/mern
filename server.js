c15dc3777a712a68222e78f5460b744317764157
Ardie C. Derrayal <31508884+arethey@users.noreply.github.com>
2019-02-01 17:28:35 +0800

    Add files via upload

---
+++ server.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const cors = require("cors");
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const path = require('path');
+
+const users = require('./routes/api/users');
+
+const app = express();
+
+// Bodyparser Middleware
+app.use(bodyParser.json());
+app.use(cors())
+app.use(
+  bodyParser.urlencoded({
+    extended: false
+  })
+)
+
+// DB Config
+const db = require('./config/keys').mongoURI;
+
+// Connect to Mongo
+mongoose
+  .connect(db, { useNewUrlParser: true }) // Adding new mongo url parser
+  .then(() => console.log('MongoDB Connected...'))
+  .catch(err => console.log(err));
+
+// Use Routes
+app.use('/api/users', users);
+
+// Serve static assets if in production
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`Server started on port ${port}`));
